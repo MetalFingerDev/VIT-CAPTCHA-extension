1c3402b1585997d45581aa70d286149df805a333
Metal Mask <dev.prajwalp@gmail.com>
2025-08-31 00:25:20 +0530

    finalizing start_server files

--- start_server.bat
+++ start_server.bat
@@ -1,3 +1,20 @@
 @echo off
+ECHO Checking for virtual environment...
+
+REM Check if the venv directory exists. If not, create it.
+if not exist venv (
+    ECHO Creating virtual environment...
+    python -m venv venv
+)
+
+REM Activate the virtual environment
+ECHO Activating virtual environment...
 call venv\Scripts\activate
+
+REM Install dependencies from requirements.txt
+ECHO Installing dependencies...
+pip install -r requirements.txt
+
+REM Run the Python application
+ECHO Starting Flask server...
 python app.py
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+echo "Checking for virtual environment..."
+
+# Check if the venv directory exists. If not, create it.
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
 source venv/bin/activate
+
+# Install dependencies from requirements.txt
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+# Run the Python application
+echo "Starting Flask server..."
 python3 app.py
